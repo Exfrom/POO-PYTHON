f9f78ebbdf352719715db4d110c284486884784d
AlejoMiran <josealejandromiranda00@gmail.com>
2023-06-17 11:48:02 -0500

    desarrolloretos

---
+++ __pycache__/salud/inicio.py
@@ -0,0 +1,7 @@
+from Persona import Persona
+Persona persona1=Persona()
+persona1.pedirDatos()
+
+
+
+
--- __pycache__/salud/persona.py
+++ __pycache__/salud/persona.py
@@ -1,22 +1,32 @@
  
-class persona:
+class Persona:
 
-#Metodo
-    def __init__(self,tipoDoc,nombre,apellidos,peso,estatura,edad,genero):
-        self.tipoDoc = tipoDoc
+#Metodos
+    def __init__(self):
+        """self.tipoDoc = tipoDoc
+        self.documento= documento
         self.nombre = nombre
         self.apellidos =apellidos
         self.peso =peso
         self.estatura = estatura
         self.edad = edad
-        self.genero = genero
+        self.genero = genero"""
 
     def pedirDatos(self):
         self.tipoDoc = input("Tipo de documento: ")
         self.documento = input("Número de documento: ")
         self.nombre = input("Nombre: ")
         self.apellidos = input("Apellidos")
-        self.peso = float(input("Peso en kg: "))
-        self.estatura = float(input("Estura en metros: "))
+        self.peso = int(input("Peso en kg: "))
+        self.estatura = int(input("Estura en metros: "))
         self.edad = int(input("edad: "))
         self.genero = int("genero: ")
+    
+    """def mostrarPersona(self):
+        print("Tipo de documento:", self.tipoDoc)
+        print("Nombre de documento:", self.nombre)
+        print("Apellidos de documento:", self.apellidos)
+        print("Peso en kg:", self.peso)
+        print("Estatura en metros:", self.estatura)
+        print("Edad de documento: ", self.edad)
+        print("Genero: ",self.genero)"""
