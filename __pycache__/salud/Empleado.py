8c8e6cab7b14bd90f84b8d402e18319b505e8b79
Exfrom <dscelis@soy.sena.edu.co>
2023-06-30 12:47:08 -0500

    correccion ejercicio

--- __pycache__/salud/Empleado.py
+++ __pycache__/salud/Empleado.py
@@ -1,13 +1,20 @@
 from Persona import Persona
 
 class Empleado(Persona):
-    
-    def __init__(self):
-        self.cargo = ""
-        self.valorHora = ""
-        self.horasTrabajadas = ""
-        self.departamento = ""
-        
+    cargo = ""
+    valorHora = 0
+    horasTrabajadas = 0 
+    departamento=""
+    honorarios = 0
+  
+    def __init__(self,tipoDoc,documento,nombre,apellido,peso,estatura,edad,sexo,
+                 cargo,valorHora,horasTrabajadas,departamento ):
+        super().__init__(tipoDoc,documento,nombre,apellido,peso,estatura,edad,sexo)
+        self.cargo= cargo
+        self.valorHora= valorHora
+        self.horasTrabajadas= horasTrabajadas
+        self.departamento= departamento
+       
     def pedirDatos(self):
         self.cargo = input("Ingrese su cargo: ")
         self.valorHora = int(input("Ingrese el valor de la hora: "))
@@ -16,6 +23,6 @@ class Empleado(Persona):
     def calcularHonorarios(self):
         honorarios = (self.valorHora * self.horasTrabajadas)-0.00966
         return honorarios
-    
+        
     def imprimirEmpleado(self):
-        print(f"El empleado {self.nombre}{self.apellido} identificado con {self.tipoDoc} numero: {self.documento} del departamento: {self.departamento} en el cargo: {self.cargo} trabajo: {self.horasTrabajadas} a un valor de {self.valorHora} la hora.")
+        print(f"El empleado {self.nombre}{self.apellido} identificado con {self.tipoDoc} numero: {self.documento} del departamento: {self.departamento} en el cargo: {self.cargo} trabajo: {self.horasTrabajadas} a un valor de {self.valorHora} la hora y un total de honorarios a pagar de {self.calcularHonorarios()}")
--- __pycache__/salud/inicio.py
+++ __pycache__/salud/inicio.py
@@ -9,20 +9,20 @@ Persona1.mostrarPersona()
 Persona1.calcularImc()
 Persona1.mayorEdad()
 
-Persona2 = Persona()
+#Persona2 = Persona()
 
-Persona2.pedirDatos()
-Persona2.mostrarPersona()
-Persona2.calcularImc()
-Persona2.mayorEdad()
+#Persona2.pedirDatos()
+#Persona2.mostrarPersona()
+#Persona2.calcularImc()
+#Persona2.mayorEdad()
 
 #Empleado:
 
-empleado1 = Empleado()
+empleado1 = Empleado("cedula", 20897871,"diana","celis",45,38,"femenini","enfermera",3000,4, "enfermeria")
 
 empleado1.pedirDatos()
 empleado1.calcularHonorarios()
-empleado1.imprimirEmpleado
-
-
+empleado1.imprimirEmpleado()
+ 
+ 
 
